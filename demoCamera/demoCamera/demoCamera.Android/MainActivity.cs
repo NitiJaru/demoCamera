f824e1991e5c73e8b73e717e711a05af8a9a69ee
Earn <earndarkness@perfenterprise.com>
2019-06-25 14:27:29 +0700

    Implement pick photo from gallery

--- demoCamera/demoCamera/demoCamera.Android/MainActivity.cs
+++ demoCamera/demoCamera/demoCamera.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace demoCamera.Droid
 {
@@ -18,6 +19,7 @@ namespace demoCamera.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
--- demoCamera/demoCamera/demoCamera/MainPage.xaml.cs
+++ demoCamera/demoCamera/demoCamera/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,28 @@ namespace demoCamera
         public MainPage()
         {
             InitializeComponent();
+            pickPicButton.Clicked += PickPicButton_Clicked;
+        }
+
+        private async void PickPicButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            var setting = new PickMediaOptions
+            {
+                CompressionQuality = 50
+            };
+
+            var response = await CrossMedia.Current.PickPhotoAsync(setting);
+            myImage.Source = response?.Path;
+
+            // Get base64
+            using (MemoryStream ms = new MemoryStream())
+            {
+                response.GetStream().CopyTo(ms);
+                var bytes = ms.ToArray();
+
+                var base64 = Convert.ToBase64String(bytes);
+            }
         }
     }
 }
